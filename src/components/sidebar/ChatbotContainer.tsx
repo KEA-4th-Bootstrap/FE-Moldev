581ba5df47c943715bf8ba18a4fda4cd1f04ddad
Ginieee <kaj1226@naver.com>
2024-04-19 23:18:34 +0900

    ✨ [Feat] 챗봇 컴포넌트에 챗봇 API 연동

--- src/components/sidebar/ChatbotContainer.tsx
+++ src/components/sidebar/ChatbotContainer.tsx
@@ -1,27 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ChatMessageType } from '../../data/type';
 import UserMessageContainer from '../chatbotContainer/UserMessageContainer';
 import ChatbotMessageContainer from '../chatbotContainer/ChatbotMessageContainer';
 import Send from '../../assets/icons/icon_send.svg?react';
+import { getChatbotAnswerApi } from '../../api/chatbotApi';
+import { useQuery } from 'react-query';
+import ChatbotLoadingContainer from '../chatbotContainer/ChatbotLoadingContainer';
 
 const ChatbotContainer = ({ memberName }: { memberName: string }) => {
   const [messages, setMessages] = useState<ChatMessageType[]>([]);
+  const [inputMessage, setInputMessage] = useState<string>('');
+  const { data, isLoading, isFetching, error, refetch } = useQuery(
+    'chatbotAnswer',
+    () => getChatbotAnswerApi(inputMessage, 99),
+    {
+      enabled: false,
+      onSuccess: (data) => {
+        setMessages((prev) => [
+          ...prev,
+          { id: prev.length + 1, message: data.data.message, isUser: false },
+        ]);
+      },
+    },
+  );
+
+  const handleSendMessage = async () => {
+    setMessages((prev) => [...prev, { id: prev.length + 1, message: inputMessage, isUser: true }]);
+    setInputMessage('');
+    refetch();
+  };
 
   useEffect(() => {
-    setMessages([
-      { id: 1, message: '안녕하세요! 무엇을 도와드릴까요?', isUser: false },
-      { id: 2, message: '안녕하세요! 무엇을 도와드릴까요?', isUser: true },
-      { id: 3, message: '안녕하세요! 무엇을 도와드릴까요?', isUser: false },
-      { id: 4, message: '안녕하세요! 무엇을 도와드릴까요?', isUser: true },
-      { id: 5, message: '안녕하세요! 무엇을 도와드릴까요?', isUser: false },
-    ]);
-  }, []);
+    console.log('Check API Response');
+    console.log(data);
+    console.log(isLoading);
+    console.log(error);
+    // if (data) {
+    //   setMessages((prev) => [...prev, { id: prev.length + 1, message: data.data, isUser: false }]);
+    // }
+  }, [data, isLoading, error]);
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-start gap-y-30">
       <div className="w-full flex flex-col items-start justify-center text-24 font-semibold pt-62">
         <div>{memberName}님의 섬 🏝️</div>
       </div>
-      <div className="grow w-full flex flex-col items-center justify-start">
+      <div className="grow w-full flex flex-col items-center justify-start overflow-y-auto gap-y-10">
         {messages.map((message) =>
           message.isUser ? (
             <UserMessageContainer key={message.id} message={message.message} />
@@ -29,13 +53,17 @@ const ChatbotContainer = ({ memberName }: { memberName: string }) => {
             <ChatbotMessageContainer key={message.id} message={message.message} />
           ),
         )}
+        {(isLoading || isFetching) && <ChatbotLoadingContainer />}
       </div>
       <div className="shrink-0 w-full flex items-center justify-center gap-x-20 pb-10">
         <input
           type="text"
           className="grow bg-gray-50 rounded-3xl outline-none border-none px-16 py-8"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          // onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
         />
-        <Send className="w-[24px] h-[24px] cursor-pointer" />
+        <Send className="w-[24px] h-[24px] cursor-pointer" onClick={handleSendMessage} />
       </div>
     </div>
   );

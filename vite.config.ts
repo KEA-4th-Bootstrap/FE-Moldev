6eefd7fd3a6bf17f92feb5af8f46e2ca2fe1de25
Ginieee <kaj1226@naver.com>
2024-04-19 10:22:45 +0900

    🐛 [Fix] 504 Outdated Optimize Dep 에러 수정 2차

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  optimizeDeps: {
+    include: ['three'],
+    exclude: ['three/examples/jsm/loaders/GLTFLoader'],
+  },
 });

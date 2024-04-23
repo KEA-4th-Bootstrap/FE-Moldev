fb08f688a8c257cdb57c1757a4a54b04d2f8deee
Ginieee <kaj1226@naver.com>
2024-04-23 17:27:01 +0900

    ✨ [Feat] 로그인/회원가입 모달 버튼클릭 이벤트 추가

--- src/components/joinContainer/JoinStep1Wrapper.tsx
+++ src/components/joinContainer/JoinStep1Wrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import FillButton from '../common/FillButton';
 
 const JoinStep1Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
@@ -6,15 +6,21 @@ const JoinStep1Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
   const [authNumber, setAuthNumber] = useState('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
+  const [isEmailSend, setIsEmailSend] = useState(false);
+  const [isAuthNumberCheck, setIsAuthNumberCheck] = useState(false);
 
   const checkIsAbleToAuth = () => {
     return email.length > 0;
   };
 
+  const checkIsAbleToConfirm = () => {
+    return authNumber.length > 0 && isEmailSend;
+  };
+
   return (
     <>
-      <div className="w-full flex flex-col items-center justify-start gap-y-32 py-32 px-8">
-        <div className="w-full flex flex-col items-center justify-start gap-y-6">
+      <div className="w-full flex flex-col items-center justify-start gap-y-40 py-32 px-8">
+        <div className="w-full flex flex-col items-center justify-start gap-y-6 relative">
           <div className="text-14 font-semibold pb-10 w-full flex items-center justify-start">
             이메일
           </div>
@@ -25,15 +31,21 @@ const JoinStep1Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
               onChange={(e) => setEmail(e.target.value)}
             />
             <button
-              className="flex items-center justify-center rounded-button bg-main py-8 px-14 text-14 text-white border-none outline-none active:border-none active:outline-none focus:border-none focus:outline-none hover:border-none hover:outline-none disabled:bg-gray-100 transition-all duration-200 ease-in-out"
+              onClick={() => setIsEmailSend(true)}
+              className="flex items-center justify-center rounded-button bg-main py-8 px-14 text-14 text-white border-none outline-none active:border-none active:outline-none focus:border-none focus:outline-none hover:border-none hover:outline-none hover:bg-dark-300 disabled:bg-gray-100 transition-all duration-200 ease-in-out"
               disabled={!checkIsAbleToAuth()}
             >
               인증번호 전송
             </button>
           </div>
           <div className="w-full h-px bg-gray-200" />
+          <div
+            className={`absolute -bottom-5 left-0 text-12 text-main ${isEmailSend ? 'block' : 'hidden'}`}
+          >
+            이메일로 인증번호가 전송되었습니다.
+          </div>
         </div>
-        <div className="w-full flex flex-col items-center justify-start gap-y-6">
+        <div className="w-full flex flex-col items-center justify-start gap-y-6 relative">
           <div className="text-14 font-semibold pb-10 w-full flex items-center justify-start">
             인증번호
           </div>
@@ -43,17 +55,27 @@ const JoinStep1Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
               value={authNumber}
               onChange={(e) => setAuthNumber(e.target.value)}
             />
-            <button className="flex items-center justify-center rounded-button bg-main py-8 px-14 text-14 text-white border-none outline-none active:border-none active:outline-none focus:border-none focus:outline-none hover:border-none hover:outline-none">
+            <button
+              onClick={() => setIsAuthNumberCheck(true)}
+              className="flex items-center justify-center rounded-button bg-main py-8 px-14 text-14 text-white border-none outline-none active:border-none active:outline-none focus:border-none focus:outline-none hover:border-none hover:outline-none hover:bg-dark-300 disabled:bg-gray-100 transition-all duration-200 ease-in-out"
+              disabled={!checkIsAbleToConfirm()}
+            >
               인증번호 확인
             </button>
           </div>
           <div className="w-full h-px bg-gray-200" />
+          <div
+            className={`absolute -bottom-5 left-0 text-12 text-main ${isAuthNumberCheck ? 'block' : 'hidden'}`}
+          >
+            인증번호 확인이 완료되었습니다.
+          </div>
         </div>
         <div className="w-full flex flex-col items-center justify-start gap-y-6">
           <div className="text-14 font-semibold pb-10 w-full flex items-center justify-start">
             비밀번호
           </div>
           <input
+            type="password"
             className="w-full flex items-center justify-start p-8 outline-none border-none "
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -65,6 +87,7 @@ const JoinStep1Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
             비밀번호 확인
           </div>
           <input
+            type="password"
             className="w-full flex items-center justify-start p-8 outline-none border-none"
             value={passwordCheck}
             onChange={(e) => setPasswordCheck(e.target.value)}
--- src/components/joinContainer/JoinStep2Wrapper.tsx
+++ src/components/joinContainer/JoinStep2Wrapper.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import FillButton from '../common/FillButton';
 
 const JoinStep2Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
   const [molDevId, setMolDevId] = useState('');
   const [userName, setUserName] = useState('');
   const [islandName, setIslandName] = useState('');
+  const [checkName, setCheckName] = useState(false);
 
   return (
     <>
       <div className="w-full flex flex-col items-center justify-start gap-y-32 py-32 px-8">
-        <div className="w-full flex flex-col items-center justify-start gap-y-6">
+        <div className="w-full flex flex-col items-center justify-start gap-y-6 relative">
           <div className="text-14 font-semibold pb-10 w-full flex items-center justify-start">
             몰디브 아이디
           </div>
@@ -22,11 +23,17 @@ const JoinStep2Wrapper = ({ onClickNext }: { onClickNext: () => void }) => {
             <button
               className="flex items-center justify-center rounded-button bg-main py-8 px-14 text-14 text-white border-none outline-none active:border-none active:outline-none focus:border-none focus:outline-none hover:border-none hover:outline-none disabled:bg-gray-100 transition-all duration-200 ease-in-out"
               disabled={!(molDevId.length > 0)}
+              onClick={() => setCheckName(true)}
             >
               중복 확인
             </button>
           </div>
           <div className="w-full h-px bg-gray-200" />
+          <div
+            className={`absolute -bottom-5 left-0 text-12 text-main ${checkName ? 'block' : 'hidden'}`}
+          >
+            몰디브 아이디 중복 확인이 완료되었습니다.
+          </div>
         </div>
         <div className="w-full flex flex-col items-center justify-start gap-y-6">
           <div className="text-14 font-semibold pb-10 w-full flex items-center justify-start">

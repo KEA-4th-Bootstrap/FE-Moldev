d111da37c099e1dceb5374ccab327d87f44ea51a
Ginieee <kaj1226@naver.com>
2024-05-07 03:09:04 +0900

    ♻️ [Refactor] useSearch 훅 연결

--- src/components/sidebar/SearchContainer.tsx
+++ src/components/sidebar/SearchContainer.tsx
@@ -1,58 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { ArticleListItemType, SearchType, UserListType } from '../../data/type';
 import SearchInputBox from '../searchContainer/SearchInputBox';
 import SearchUserResultContainer from '../searchContainer/SearchUserResultContainer';
 import ArticleListItemContainer from '../articleListContainer/ArticleListItemContainer';
+import useSearch from '../../hooks/sidebar/search/useSearch';
 
 const SearchContainer = () => {
-  const [searchType, setSearchType] = useState<SearchType>('article');
-  const [searchArticleResult, setSearchArticleResult] = useState<ArticleListItemType[]>([]);
-  const [searchUserResult, setSearchUserResult] = useState<UserListType[]>([]);
-
-  useEffect(() => {
-    setSearchUserResult([
-      {
-        img: 'https://media.bunjang.co.kr/product/252438211_1_1707301834_w360.jpg',
-        name: '김철수',
-        introduce: '복복복복복복복두더지입니다어쩌구저쩌구저쩌구',
-        visit: 100,
-        updated: '2021-10-10',
-      },
-      {
-        img: 'https://media.bunjang.co.kr/product/252438211_1_1707301834_w360.jpg',
-        name: '김철수',
-        introduce: '복복복복복복복두더지입니다어쩌구저쩌구저쩌구',
-        visit: 100,
-        updated: '2021-10-10',
-      },
-    ]);
-    setSearchArticleResult([
-      {
-        id: 1,
-        title: '제목',
-        content: '내용',
-        createdAt: '2021-10-10',
-        visit: 100,
-        category: 'activity',
-        img: 'https://media.bunjang.co.kr/product/252438211_1_1707301834_w360.jpg',
-        userName: '김철수',
-      },
-      {
-        id: 2,
-        title: '복복복복',
-        content: '어느 날 복복두더지가 어쩌구저쩌구 어진식당 어쩌구 저쩌구 어쩌구 저쩌구 ',
-        createdAt: '2021-10-10',
-        visit: 100,
-        category: 'activity',
-        img: 'https://media.bunjang.co.kr/product/252438211_1_1707301834_w360.jpg',
-        userName: '김철수',
-      },
-    ]);
-  }, []);
-
-  useEffect(() => {
-    setSearchType('article');
-  }, []);
+  const { searchType, searchArticleResult, searchUserResult, setSearchType } = useSearch();
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-start gap-y-24">
@@ -60,7 +12,7 @@ const SearchContainer = () => {
       <div className="w-full flex flex-col items-center justify-start">
         {searchType === 'article' &&
           searchArticleResult.map((item, idx) => (
-            <ArticleListItemContainer key={item.id + idx} item={item} />
+            <ArticleListItemContainer key={item.id + idx} item={item} moldevId={item.moldevId} />
           ))}
         {searchType === 'user' &&
           searchUserResult.map((item, idx) => (

cf945f280d39c6b1b3cac4c03e45167a182cf6e6
Ginieee <kaj1226@naver.com>
2024-05-07 01:06:54 +0900

    ♻️ [Refactor] useLoginPage 커스텀훅 연결

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,35 +1,10 @@
-import { useEffect, useState } from 'react';
 import BlurBackgroundContainer from '../components/common/BlurBackgroundContainer';
 import LoginContainer from '../components/loginContainer/LoginContainer';
-import { useNavigate } from 'react-router';
+import useLoginPage from '../hooks/login/useLoginPage';
 
 const LoginPage = () => {
-  const navigation = useNavigate();
-  const [isShow, setIsShow] = useState(false);
-  const [childIsShow, setChildIsShow] = useState(false);
+  const { isShow, childIsShow, setChildIsShow } = useLoginPage();
 
-  useEffect(() => {
-    setIsShow(true);
-    const timer = setTimeout(() => {
-      setChildIsShow(true);
-    }, 100);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!childIsShow) {
-      const timer = setTimeout(() => {
-        navigation(-1);
-      }, 300);
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [childIsShow, navigation]);
   return (
     <BlurBackgroundContainer isShow={isShow} onClick={() => setChildIsShow(false)}>
       <LoginContainer isShow={childIsShow} setIsShow={setChildIsShow} />
